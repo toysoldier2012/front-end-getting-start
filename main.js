f1e4160a2c60c8d3a522eeb089e51c663d1ef64d
toysoldier2012 <toysoldier2012@gmail.com>
2023-09-19 15:55:51 +0200

    array and object

--- main.js
+++ main.js
@@ -24,6 +24,19 @@ console.log("Hello " + customer);
 const constant = 2000;
 let variable = 200;
 
+/**Array**/
+
+let array = [0,1,2,3,4,5,6,7];
+array.push(10);
+array.unshift(-1);
+console.log(array);
+let arrayConcat = array.concat([5646,879,12]);
+console.log(arrayConcat);
+
+/**Object**/
+let student = {"name": "John", "yearOfBirth": 1990, "country":"Italy"};
+console.log(student.name);
+
 /**Greeting section**/
 
 const greetingText = "Good morning!"
@@ -37,3 +50,5 @@ console.log(weatherText);
 
 document.querySelector('#greeting').innerHTML = greetingText;
 document.querySelector('#weather').innerHTML = weatherText;
+
+
